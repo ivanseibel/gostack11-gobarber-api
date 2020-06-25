be2d2400f33bfb07b3ccc0b00271933d94eed926
Ivan Seibel <ivanseibel@gmail.com>
2020-06-25 15:59:19 -0300

    Create appointments route file

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  const {name, email} = request.body;
-
-  const user = {
-    name,
-    email
-  }
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Para o alto e avante! 🚀');
