eb7e60e847572d671a1ed0e9bf4e8f35dbb30fc7
Ivan Seibel <ivanseibel@gmail.com>
2020-06-25 18:23:16 -0300

    Create appointments repository

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,26 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const newAppointment = new Appointment(provider, date);
+    this.appointments.push(newAppointment);
+    return newAppointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const appointment = this.appointments.find(item =>
+      isEqual(item.date, date),
+    );
+
+    return appointment || null;
+  }
+}
+
+export default new AppointmentRepository();
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,29 +1,22 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const timeUnavailable = appointments.find(appointment =>
-    isEqual(appointment.date, parsedDate),
-  );
+  const timeUnavailable = AppointmentRepository.findByDate(parsedDate);
 
   if (timeUnavailable) {
     return response.status(400).json({ message: 'Time is not available' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = AppointmentRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
