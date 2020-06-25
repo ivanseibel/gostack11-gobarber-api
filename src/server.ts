e8b29e34cbf457e51f342a4023e4560d337045c0
Ivan Seibel <ivanseibel@gmail.com>
2020-06-25 15:10:14 -0300

    Create debug config

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,19 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  const {name, email} = request.body;
+
+  const user = {
+    name,
+    email
+  }
+
+  return response.json(user);
+});
 
 app.listen(3333, () => {
-  console.log('🚀 Para o alto e avante!');
+  console.log('🚀 Para o alto e avante! 🚀');
 });
