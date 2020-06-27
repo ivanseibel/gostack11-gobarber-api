a266639cf36f4b23e821e72576c89d74ae3b5afd
Ivan Seibel <ivanseibel@gmail.com>
2020-06-27 20:33:18 -0300

    Add static file server to avatars

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/uploads';
 
 import './database';
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/files', express.static(uploadConfig.avatarDir));
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
