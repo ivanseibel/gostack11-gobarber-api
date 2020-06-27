e98934a5b8d28f58254f4ab52a93068944aa32a5
Ivan Seibel <ivanseibel@gmail.com>
2020-06-27 14:16:42 -0300

    Implement jwt in session - Install jsonwebtoken - Update authenticateuser service - Update sessions route

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -27,7 +29,12 @@ class AuthenticateUserService {
       throw Error('Incorrect email/password combination');
     }
 
-    return { user };
+    const token = sign({}, '4ac98544361a23855fd57d5e04be83c9', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
