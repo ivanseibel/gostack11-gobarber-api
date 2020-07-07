a980df6a65a23dc4c79f64e11386be4d4250628d
Ivan Seibel <ivanseibel@gmail.com>
2020-07-07 17:56:28 -0300

    Fake test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,9 @@
-test('teste', () => {
-  expect(1 + 2).toBe(3);
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  // it('should not be able to create a new appointment', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
