8f5a19dcb599f55409d173e4742e2caaff4320fa
Ivan Seibel <ivanseibel@gmail.com>
2020-07-07 11:24:38 -0300

    Small refactories

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -3,7 +3,7 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '../entities/Appointment';
 
-class AppointmentRepository implements IAppointmentsRepository {
+class AppointmentsRepository implements IAppointmentsRepository {
   private ormRepository: Repository<Appointment>;
 
   constructor() {
@@ -11,11 +11,7 @@ class AppointmentRepository implements IAppointmentsRepository {
   }
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const appointment = await this.ormRepository.findOne({
-      where: { date },
-    });
-
-    return appointment;
+    return this.ormRepository.findOne({ where: { date } });
   }
 
   public async create({
@@ -23,10 +19,9 @@ class AppointmentRepository implements IAppointmentsRepository {
     date,
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = this.ormRepository.create({ provider_id, date });
-    await this.ormRepository.save(appointment);
 
-    return appointment;
+    return this.ormRepository.save(appointment);
   }
 }
 
-export default AppointmentRepository;
+export default AppointmentsRepository;
