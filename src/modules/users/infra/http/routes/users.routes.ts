1524807806ba7615fdafc38997817173cf79b534
Ivan Seibel <ivanseibel@gmail.com>
2020-07-07 16:21:02 -0300

    Implement move code from routes to controllers

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,53 +1,24 @@
 import { Router } from 'express';
 
 import multer from 'multer';
-import CreateUserService from '@modules/users/services/CreateUserService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import uploadsConfig from '@config/uploads';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-import { container } from 'tsyringe';
+import UserAvatarController from '../controllers/UserAvatarController';
+import UsersController from '../controllers/UsersController';
 
 const usersRouter = Router();
+const userAvatarController = new UserAvatarController();
+const usersController = new UsersController();
 
 const upload = multer(uploadsConfig);
-
-// usersRouter.get('/', async (request, response) => {
-//   const usersRepository = getRepository(User);
-//   const users = await usersRepository.find();
-//   return response.json(users);
-// });
 
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-    const user = await updateUserAvatar.execute({
-      avatarFileName: request.file.filename,
-      user_id: request.user.id,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-  },
+  userAvatarController.update,
 );
 
-usersRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
-
-  const createUser = container.resolve(CreateUserService);
-  const user = await createUser.execute({
-    name,
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json(user);
-});
+usersRouter.post('/', usersController.create);
 
 export default usersRouter;
