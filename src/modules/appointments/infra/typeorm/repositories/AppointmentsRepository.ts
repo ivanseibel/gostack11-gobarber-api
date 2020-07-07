983a098978aca2254ccd8a6277fb26f73390b3ff
Ivan Seibel <ivanseibel@gmail.com>
2020-07-07 07:54:52 -0300

    Implement Liskov Substitution Principle in repositories

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,14 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
 @EntityRepository(Appointment)
-class AppointmentRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentRepository extends Repository<IAppointmentsRepository> {
+  public async findByDate(
+    date: Date,
+  ): Promise<IAppointmentsRepository | undefined> {
     const appointment = await this.findOne({
       where: { date },
     });
 
-    return appointment || null;
+    return appointment;
   }
 }
 
