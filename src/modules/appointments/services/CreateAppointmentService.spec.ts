61dac3409c85f8d3def997ca4199e2d9c807a245
Ivan Seibel <ivanseibel@gmail.com>
2020-07-07 18:34:07 -0300

    Create unitary test to CreateAppointmentService

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,6 +1,20 @@
+import FakeAppointmentRepository from '../repositories/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new appointment', async () => {
+    const appointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      appointmentRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      provider_id: '123456',
+      date: new Date(),
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456');
   });
 
   // it('should not be able to create a new appointment', () => {
