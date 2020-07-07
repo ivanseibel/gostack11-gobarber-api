fe890174f7456ec50480acfee9f3a2a57e870ed5
Ivan Seibel <ivanseibel@gmail.com>
2020-07-07 19:32:21 -0300

    Implement test to verify if is possible to create 2 appointments to same time

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentRepository from '../repositories/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,7 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123456');
   });
 
-  // it('should not be able to create a new appointment', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create a new appointment', async () => {
+    const appointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      appointmentRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      provider_id: '123456',
+      date: appointmentDate,
+    });
+
+    expect(
+      createAppointment.execute({
+        provider_id: '123456',
+        date: appointmentDate,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
