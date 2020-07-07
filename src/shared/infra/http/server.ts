a0200dc87d51ed454b9e5fa8744b3aee981636b5
Ivan Seibel <ivanseibel@gmail.com>
2020-07-07 11:13:39 -0300

    Add more information to internal server error messages

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -23,9 +23,10 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       .json({ status: 'error', message: err.message });
   }
 
-  return response
-    .status(500)
-    .json({ status: 'error', message: 'Internal server error' });
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error: ${err.message}`,
+  });
 });
 
 app.use('/files', express.static(uploadConfig.avatarDir));
