0a251800a2842b59bf06027ee1f9cca9acd0ada7
Ivan Seibel <ivanseibel@gmail.com>
2020-07-08 08:12:13 -0300

    Add 2 new tests to AuthenticateUserService

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
@@ -30,5 +31,61 @@ describe('AuthenticateUserService', () => {
 
     expect(result).toHaveProperty('token');
     expect(result.user).toEqual(user);
+  });
+
+  it('should not be able get an access token with nonexistent user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    let error;
+
+    try {
+      await authenticateUser.execute({
+        email: 'johndoe@email.com',
+        password: '123456',
+      });
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error).toBeInstanceOf(AppError);
+  });
+
+  it('should not be able get an access token with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    let error;
+
+    try {
+      await authenticateUser.execute({
+        email: 'johndoe@email.com',
+        password: 'wrong-password',
+      });
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error).toBeInstanceOf(AppError);
   });
 });
