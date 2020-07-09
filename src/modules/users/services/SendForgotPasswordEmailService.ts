75d46b793e72d484c2e3e8e2a7f50dc563b58ecf
Ivan Seibel <ivanseibel@gmail.com>
2020-07-09 17:48:11 -0300

    Small updates - Add await to sendMail method - Capture token and send with body email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -28,9 +28,12 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(data.email, 'Recovery email.');
+    await this.mailProvider.sendMail(
+      data.email,
+      `Recovery email. Token: ${token}`,
+    );
   }
 }
 
