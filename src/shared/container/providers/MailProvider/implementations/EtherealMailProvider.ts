e16c650032f93d256d12ae17d44b20b4ee960fcd
Ivan Seibel <ivanseibel@gmail.com>
2020-07-09 21:04:19 -0300

    Integrate with MailTemplateProvider

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,16 +1,23 @@
 import nodemailer, { Transporter } from 'nodemailer';
 
+import { injectable, inject } from 'tsyringe';
 import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
 
 interface IMessage {
   to: string;
   body: string;
 }
 
-export default class EtherealMailProvider implements IMailProvider {
+@injectable()
+class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
     nodemailer.createTestAccount().then(account => {
       const transporter = nodemailer.createTransport({
         host: account.smtp.host,
@@ -26,15 +33,29 @@ export default class EtherealMailProvider implements IMailProvider {
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: 'GoBarber Team <team@gobarber.com>',
-      to,
-      subject: 'Password recover',
-      text: body,
+      from: {
+        address: from?.email || 'team@gobarber.com',
+        name: from?.name || 'GoBarber Team',
+      },
+      to: {
+        address: to.email,
+        name: to.name,
+      },
+      subject,
+      text: 'Test',
+      html: await this.mailTemplateProvider.parse(templateData),
     });
 
     console.log('Message sent: %', message.messageId);
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
   }
 }
+
+export default EtherealMailProvider;
