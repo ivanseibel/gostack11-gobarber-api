23c608b69128ffbcf56e5fc2e501ed6f3e321499
Ivan Seibel <ivanseibel@gmail.com>
2020-07-10 10:58:20 -0300

    Update to avoid error after change to read views

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,8 +1,7 @@
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 
 export default class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse(data: IParseMailTemplateDTO): Promise<string> {
-    return data.template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
