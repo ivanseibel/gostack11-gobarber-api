bd02d0c0af08d817f79a743c6bbd23a7a37c829a
Ivan Seibel <ivanseibel@gmail.com>
2020-07-11 17:00:14 -0300

    Add method findByMonth

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,6 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Between } from 'typeorm';
+import { startOfMonth, endOfMonth } from 'date-fns';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindByMonthDTO from '@modules/appointments/dtos/IFindByMonthDTO';
 import Appointment from '../entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentsRepository {
@@ -21,6 +23,21 @@ class AppointmentsRepository implements IAppointmentsRepository {
     const appointment = this.ormRepository.create({ provider_id, date });
 
     return this.ormRepository.save(appointment);
+  }
+
+  public async findByMonth({
+    provider_id,
+    month,
+    year,
+  }: IFindByMonthDTO): Promise<Appointment[]> {
+    const firstDay = startOfMonth(new Date(year, month - 1, 1));
+    const lastDay = endOfMonth(new Date(year, month - 1, 1));
+
+    const where = provider_id
+      ? { id: provider_id, date: Between(firstDay, lastDay) }
+      : { date: Between(firstDay, lastDay) };
+
+    return this.ormRepository.find(where);
   }
 }
 
