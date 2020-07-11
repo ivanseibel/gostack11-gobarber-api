185c40884534edc4c1825268c739405d03530683
Ivan Seibel <ivanseibel@gmail.com>
2020-07-11 17:01:06 -0300

    Add method findByMonth

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,13 @@
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
+import IFindByMonthDTO from '../dtos/IFindByMonthDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findByMonth({
+    provider_id,
+    month,
+    year,
+  }: IFindByMonthDTO): Promise<Appointment[]>;
 }
