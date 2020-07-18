0af718a3cc03e1c07f92ad9c0a5ad97ea71ba564
Ivan Seibel <ivanseibel@gmail.com>
2020-07-18 13:59:50 -0300

    Add AWS S3 config.

--- src/config/uploads.ts
+++ src/config/uploads.ts
@@ -1,18 +1,46 @@
 import path from 'path';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 
 const tmpDir = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpDir: string;
+  uploadsDir: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: unknown;
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tmpDir,
   uploadsDir: path.resolve(tmpDir, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpDir,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
-      return callback(null, fileName);
+
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpDir,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'gobarber-new-version',
     },
-  }),
-};
+  },
+} as IUploadConfig;
