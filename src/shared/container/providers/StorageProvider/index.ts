5d96044dcfdc545717226b4bb6b180c88efe419f
Ivan Seibel <ivanseibel@gmail.com>
2020-07-18 14:05:53 -0300

    Implement dinamic dependency injection.

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,13 +1,17 @@
 import { container } from 'tsyringe';
 
+import uploadsConfig from '@config/uploads';
+
 import IStorageProvider from './models/IStorageProvider';
 import DiskStorageProvider from './fakes/FakeStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.disk,
+  providers[uploadsConfig.driver],
 );
