f068eb06dee15e114a78e5a4517f20fb33999f4c
Ivan Seibel <ivanseibel@gmail.com>
2020-07-19 15:03:08 -0300

    Add cache to providers list.

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -2,18 +2,30 @@ import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IListProvidersDTO from '@modules/users/dtos/IListProvidersDTO';
 import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 @injectable()
 export default class ShowProfileService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ except_user_id }: IListProvidersDTO): Promise<User[]> {
-    const users = await this.usersRepository.listProviders({
-      except_user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${except_user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.listProviders({
+        except_user_id,
+      });
+
+      await this.cacheProvider.save(`providers-list:${except_user_id}`, users);
+    }
 
     return users;
   }
