40f1f1c55ade60346efa7e9d54ae972102d308c4
Ivan Seibel <ivanseibel@gmail.com>
2020-07-23 14:59:52 -0300

    Avoid apply request limit to avatar static server.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -14,8 +14,6 @@ import '@shared/container';
 
 const app = express();
 
-app.use(rateLimiter);
-
 app.use(cors());
 
 app.use(express.json());
@@ -23,6 +21,10 @@ app.use(express.json());
 app.use(routes);
 
 app.use(errors());
+
+app.use('/files', express.static(uploadConfig.uploadsDir));
+
+app.use(rateLimiter);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
@@ -36,8 +38,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     message: `Internal server error: ${err.message}`,
   });
 });
-
-app.use('/files', express.static(uploadConfig.uploadsDir));
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
